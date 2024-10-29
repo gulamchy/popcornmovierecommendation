e126376475d45f459244aabeb610ddd800e1ba8d
gulamchy <gulamsulaman@gmail.com>
2024-10-28 22:45:46 -0700

    Adding static

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+// }));
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Load movie data and similarity data
 let movies = [];
